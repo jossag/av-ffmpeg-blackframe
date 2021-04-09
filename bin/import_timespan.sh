94fb0fa087c62e4c4e3adfe4c9b6332082f28a8c
Jonas Sandberg <jonas.sandberg@codemill.se>
2021-04-09 12:16:02 +0200

    Add custom ingest template

---
+++ bin/import_timespan.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo "Importing black frame timespans for asset $1"
+curl -X POST "${AP_ADAPTER_URL_MP_REST_URL}/asset/$1/timespan/bulk"  -u "${AP_ADAPTER_USERNAME}:${AP_ADAPTER_PASSWORD}"  -H 'Content-Type: application/json' -d @timespans.json
